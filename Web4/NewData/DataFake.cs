82444812d143a7667c7b1d1adf67cb48f9d38191
trungnguyendev <trungvjppro23@gmail.com>
2023-10-15 01:43:02 +0700

    11

--- Web4/NewData/DataFake.cs
+++ Web4/NewData/DataFake.cs
@@ -2,5 +2,11 @@
 {
     public class DataFake
     {
+        public string name { get; set; }
+        public int age { get; set; }
+        public int id { get; set; }
+        public string name2 { get; set; }
+        public int age2 { get; set; }
+        public int id2 { get; set; }
     }
 }
